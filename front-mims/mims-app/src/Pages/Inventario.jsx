78165f17f3a9816452d8384b926985f019aac277
Cristobal Arias Barros <cristobal.arias@usach.cl>
2023-05-11 23:39:47 -0400

    [Cristobal Arias] - fix: Arreglo en bug de botones

--- front-mims/mims-app/src/Pages/Inventario.jsx
+++ front-mims/mims-app/src/Pages/Inventario.jsx
@@ -2,101 +2,105 @@
 import { Table, Button } from 'react-bootstrap';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const Inventario = () => {
     const [productos, setProductos] = useState([]);
-    const [cantidad, setCantidad] = useState(0);
-    const [precio, setPrecio] = useState(0);
-    const [productoReal, setProductoReal] = useState([]);
-    const [producto, setProducto] = useState({
-        id: null,
-        id_joya: null,
-        id_locacion: null,
-        precio : 0,
-        cantidad: 0,
-        deleted: false,
-        id_tipo_joya: null,
-    });
-
+    const [productoReal, setProductoReal] = useState(null);
+  
     const getProductoReal = async (id) => {
-        try {
-            const res = await axios.get('http://localhost:8080/inventario/' + id);
-            setProductoReal(res.data);
-        }
-        catch (error) {
-            console.log(error);
-        }
-    }
-
+      try {
+        const res = await axios.get('http://localhost:8080/inventario/' + id);
+        setProductoReal(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+  
     const getProductos = async () => {
-        try {
-            const res = await axios.get('http://localhost:8080/inventario');
-            setProductos(res.data);
-        }
-        catch (error) {
-            console.log(error);
-        }
-    }
-
+      try {
+        const res = await axios.get('http://localhost:8080/inventario');
+        setProductos(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+  
     const handleUpdate = async (productoReal) => {
-        try {
-            let url = 'http://localhost:8080/inventario/' + productoReal.id;
-            const res = await axios.put(url, productoReal);
-            if (res.status === 200) {
-                getProductos();
-            }
+      try {
+        let url = 'http://localhost:8080/inventario/' + productoReal.id;
+        const res = await axios.put(url, productoReal);
+        if (res.status === 200) {
+          getProductos();
         }
-        catch (error) {
-            console.log(error);
-        }
-    }
-
-    const handleChangeplus = (producto) => {
-        getProductoReal(producto.id);
-        productoReal.cantidad = productoReal.cantidad + 1;
-        handleUpdate(productoReal);
-    }
-
-    const handleChangeless = (producto) => {
-        getProductoReal(producto.id);
-        productoReal.cantidad = productoReal.cantidad - 1;
-        handleUpdate(productoReal);
-    }
-
+      } catch (error) {
+        console.log(error);
+      }
+    };
+  
+    const handleChangeplus = async (producto) => {
+      await getProductoReal(producto.id);
+      setProductoReal((prevState) => {
+        const updatedProductoReal = { ...prevState, cantidad: prevState.cantidad + 1 };
+        handleUpdate(updatedProductoReal);
+        return updatedProductoReal;
+      });
+    };
+  
+    const handleChangeless = async (producto) => {
+      await getProductoReal(producto.id);
+      setProductoReal((prevState) => {
+        const updatedProductoReal = { ...prevState, cantidad: prevState.cantidad - 1 };
+        handleUpdate(updatedProductoReal);
+        return updatedProductoReal;
+      });
+    };
+  
     useEffect(() => {
-        getProductos();
-    },[]);
-
+      getProductos();
+    }, []);
+  
     return (
-        <Table striped bordered hover size="sm">
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Local</th>
-                    <th>Cantidad</th>
-                    <th>Joya</th>
-                    <th>Precio</th>
-                    <th>Acciones</th>
+        <Container style={{ marginTop: '50px', textAlign: 'center' }}>
+        <h1 style={{ fontSize: '48px' }}>Inventario</h1>
+        <Row style={{ marginTop: '20px' }}>
+    <Col>
+      <Button variant="primary" style={{ marginRight: '10px' }}>Agregar</Button>
+      <Button variant="danger">Eliminar</Button>
+    </Col>
+  </Row>
+        <Table bordered hover style={{ marginTop: '20px' }}>
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>Local</th>
+                <th>Cantidad</th>
+                <th>Joya</th>
+                <th>Precio</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            {productos.map((producto, index) => (
+                <tr key={index}>
+                    <td>{index + 1}</td>
+                    <td>{producto.local}</td>
+                    <td>{producto.cantidad}</td>
+                    <td>{producto.joya}</td>
+                    <td>{producto.precio}</td>
+                    <td>
+                        <Button variant='primary' style={{ marginRight: '5px' }} onClick={() => handleChangeplus(producto)}> + </Button>
+                        <Button variant='danger' style={{ marginLeft: '5px' }} onClick={() => handleChangeless(producto)}> - </Button>
+                    </td>
                 </tr>
-            </thead>
-            <tbody>
-                {productos.map((producto, index) => (
-                    <tr key={index}>
-                        <td>{index + 1}</td>
-                        <td>{producto.local}</td>
-                        <td>{producto.cantidad}</td>
-                        <td>{producto.joya}</td>
-                        <td>{producto.precio}</td>
-                        <td>
-                            <Button variant='primary' onClick={() => handleChangeplus(producto)}> + </Button>
-                            <Button variant='danger' onClick={() => handleChangeless(producto)}> - </Button>
-                        </td>
-                    </tr>
-                ))}
+            ))}
 
-            </tbody>
-        </Table>
+        </tbody>
+    </Table>
+    </Container>
     );
-
-}
-export default Inventario;
+  };
+  
+  export default Inventario;
